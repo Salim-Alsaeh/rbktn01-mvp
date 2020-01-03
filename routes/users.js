3b02a79b32f615cf5e93f24705784a3506a613fd
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-03 12:50:56 +0100

    hashing password and jwt server response

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const { check, validationResult } = require('express-validator');
 
@@ -14,13 +17,48 @@ router.post('/',[
     check('password', 'Please include a password with 6 or more characters').isLength({
         min: 6
     })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()})
     }
+    const {name, email, password} = req.body;
 
-    res.send('passed')
+    try {
+        let user = await User.findOne({email});
+
+        if (user) {
+            res.status(400).json({msg: "User already exists"});
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({ token })
+        })
+
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error')
+    }
 });
 
 module.exports = router;
