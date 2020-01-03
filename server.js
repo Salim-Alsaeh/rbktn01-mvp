e3ecf20c3f29712cf6be20868ddbb165a9a568d4
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-03 12:30:01 +0100

    added validation rules for users routes

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const server = require('express');
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
 
-const app = server();
+//connect to DB
+connectDB();
 
+//json body parser middleware
+app.use(express.json({extended: false}));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
